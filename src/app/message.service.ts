8bab4733bc8172f634cfcb2c9bec9f72fe20a579
pnadalini <pietro.nadalini@hotmail.com>
2019-02-11 21:50:49 -0600

    Updated project and added missing steps from the tutorial

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class MessageService {
   messages: string[] = [];
 
-  add(message: string){
+  add(message: string) {
     this.messages.push(message);
   }
 
